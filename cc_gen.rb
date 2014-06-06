1b4d3485b6caa1914b8f42a3f64f9da4b7e16540
Alexander Shvets <alexander.shvets@gmail.com>
2014-06-06 14:25:47 -0400

    Update cc_gen.rb

--- cc_gen.rb
+++ cc_gen.rb
@@ -1,50 +1,59 @@
+class CCNGenerator
 
-def generate_credit_card_number prefix, size     
-  number = prefix + "1"*(size-prefix.size)
-       
-  sum = calculate_sum number
-        
-  while (sum % 10) > 0 do
-    normalize number, prefix, (sum % 10)
-    
+  def generate prefix, suffix, size
+    number = prefix + "1"*(size-prefix.size-suffix.size) + suffix
+
+    range = (prefix.size..number.size-suffix.size-1)
+
+    normalize(number, range) until verify(number)
+
+    number
+  end
+
+  def verify number
+    (calculate_sum(number) % 10) == 0
+  end
+
+  private
+
+  def calculate_sum number
+    parity = number.size % 2
+
+    sum = 0
+    index = 0
+    number.each_char do |char|
+      sum = sum + digit(char.to_i, index, parity)
+      index = index + 1
+    end
+
+    sum
+  end
+
+  def digit digit, index, parity
+    digit = digit * 2 if index % 2 == parity
+    digit = digit - 9 if digit > 9
+
+    digit
+  end
+
+  def normalize number, range
+    parity = number.size % 2
     sum = calculate_sum number
-  end
+    diff = sum % 10
 
-  number
-end
-
-def calculate_sum number
-  parity = number.size % 2
-  
-  sum = 0
-  index = 0
-  number.each_char do |char|
-    sum = sum + digit(char.to_i, index, parity)
-    index = index + 1
-  end
-  
-  sum
-end
-
-def digit digit, index, parity
-  digit = digit * 2 if index % 2 == parity
-  digit = digit - 9 if digit > 9
-  
-  digit
-end
-
-def normalize number, prefix, diff
-  parity = number.size % 2
-   
-  (prefix.size..(number.size-1)).each do |index|
-    if index % 2 != parity && diff > 0
-      number[index] = digit((number[index, 1].to_i + 1), index, parity).to_s
-      diff = diff - 1
+    range.each do |index|
+      if index % 2 != parity && diff > 0
+        number[index] = digit((number[index, 1].to_i + 1), index, parity).to_s
+        diff = diff - 1
+      end
     end
   end
 end
-  
-cc_number = generate_credit_card_number "6334", 16
 
-p cc_number
+gen = CCNGenerator.new
 
+cc_number = gen.generate "6334", "0000", 16
+
+p "Credit Card Number: #{cc_number}"
+
+p "Verify: #{gen.verify(cc_number) ? 'OK' : "Fail"}"
